7298695fd4fabbf177bb75b1b671f3c031df6347
gfucci <gfucci_2005@hotmail.com>
2022-10-01 14:10:43 -0300

    Finaliza dashboard

--- dao/MovieDAO.php
+++ dao/MovieDAO.php
@@ -85,6 +85,27 @@
 
         public function getMovieByUserId($id) {
 
+            $movies = [];
+
+            $stmt = $this->conn->prepare("SELECT * FROM movies
+                WHERE users_id = :users_id
+                ORDER BY id DESC
+            ");
+
+            $stmt->bindParam(":users_id", $id);
+            $stmt->execute();
+
+            if ($stmt->rowCount() > 0) {
+
+                $moviesArray = $stmt->fetchAll();
+                
+                foreach ($moviesArray as $movie) {
+
+                    $movies[] = $this->buildMovie($movie);
+                }
+            }
+
+            return $movies;
         }
 
         public function findById($id) {
