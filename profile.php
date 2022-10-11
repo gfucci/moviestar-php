0fe34e39c3d7e2b108e63bc50f773fc01a788310
gfucci <gfucci_2005@hotmail.com>
2022-10-10 21:08:58 -0300

    cria perfil do usuario

--- profile.php
+++ profile.php
@@ -15,6 +15,8 @@
     //get user id
     $id = filter_input(INPUT_GET, "id");
 
+    //echo $id; exit;
+
     //get id if user is logged in
     if (empty($id)) {
 
@@ -41,13 +43,42 @@
     //get user name and image
     $getName = $user->getFullName($userData);
 
-    if ($userData->image = "") {
+    if ($userData->image == "") {
 
         $userData->image = "user.png";
     }
+
+    //movies that user added
+    $userMovies = $movieDao->getMovieByUserId($id);
 ?>
     <div id="main-container" class="container-fluid">
-        
+        <div class="col-md-8 offset-md-2">
+            <div class="row profile-container">
+                <div class="col-md-12 about-container">
+                    <h1 class="page-title">
+                        <?= $getName ?>
+                    </h1>
+                    <div id="profile-image-container" class="profile-image" style="background-image: url('<?= $BASE_URL ?>img/users/<?= $userData->image ?>')"></div>
+                    <h3 class="about-tile">Sobre:</h3>
+                    <?php if (!empty($userData->bio)): ?>
+                        <p class="profile-description"><?= $userData->bio ?></p>
+                    <?php else: ?>
+                        <p class="profile-description">O usuário não possuí bio</p>
+                    <?php endif; ?>
+                </div>
+                <div class="col-md-12 added-movies-container">
+                    <h3>Filmes que enviou:</h3>
+                    <div class="movies-container">
+                        <?php foreach ($userMovies as $movie): ?>
+                            <?php require("templates/movieCard.php"); ?>
+                        <?php endforeach; ?>
+                        <?php if (count($userMovies) === 0): ?>
+                            <p class="empty-list">O usuário ainda não enviou filmes</p>
+                        <?php endif; ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 <?php
 
